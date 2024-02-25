b8d60ecaae588a47af2a0ad37b20b73cc3ba47a8
S0LARANA <s0169513@edu.kubsu.ru>
2024-02-25 17:23:50 +0300

    Add ui, graph visualization, basic form fields

---
+++ app.py
@@ -0,0 +1,51 @@
+import sys
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QMainWindow, QWidget
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.ticker import LinearLocator
+from PyQt5 import uic
+
+class MyMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Load the UI file
+        uic.loadUi('ui\main_window.ui', self)
+
+        # Find the appropriate widget in the UI file
+        widget = self.findChild(QWidget, 'widget')  # Replace 'widget_name' with the actual name of the widget
+
+        # Create a layout for the widget
+        layout = QVBoxLayout(widget)
+
+        # Create a Matplotlib figure and plot
+        self.fig, self.ax = plt.subplots(subplot_kw={"projection": "3d"})
+        X = np.arange(-5, 5, 0.25)
+        Y = np.arange(-5, 5, 0.25)
+        X, Y = np.meshgrid(X, Y)
+        R = np.sqrt(X ** 2 + Y ** 2)
+        Z = np.sin(R)
+
+        # Plot the surface.
+        surf = self.ax.plot_surface(X, Y, Z, cmap=matplotlib.colormaps['viridis'],
+                               linewidth=0, antialiased=False)
+
+        # Customize the z axis.
+        self.ax.set_zlim(-1.01, 1.01)
+        self.ax.zaxis.set_major_locator(LinearLocator(10))
+        # A StrMethodFormatter is used automatically
+        self.ax.zaxis.set_major_formatter('{x:.02f}')
+
+        # Create a FigureCanvasQTAgg object
+        self.canvas = FigureCanvas(self.fig)
+
+        # Add the FigureCanvasQTAgg object to the layout
+        layout.addWidget(self.canvas)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyMainWindow()
+    window.show()
+    sys.exit(app.exec_())
